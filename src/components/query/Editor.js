448bd26a077607a644b02df5a2d502ce7d99a801
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-01-22 00:23:34 +0100

    [UPDATE] editor

--- src/components/query/Editor.js
+++ src/components/query/Editor.js
@@ -1,17 +1,48 @@
 import React, { useState } from 'react';
+import {apiConfig} from "../../conf/api";
 
 import {consoleStyle, editorWindow} from "./style/Editor.style"
 
 export const Editor = (props) => {
+    const [rawQuery, setRawQuery] = useState(null);
 
+    // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Editable_content/Rich-Text_Editing_in_Mozilla
+    /*
     const onPressKey = (keyObject) => {
         const { value } = keyObject.target; 
+    }*/
+
+
+    const onChangeEditor = (event) => {
+        const { value } = event.target;
+        setRawQuery(value);
     }
+
+
+    const onSubmitRawQuery = async () => {
+
+       let req = await fetch(`${apiConfig.wyrmqueryUrl}/query`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        method: "POST",
+        body: JSON.stringify({query: rawQuery})
+     })
+       let json = await req.json();
+       console.log(json);
+    }
+
+    
 
     return ( <div>
         <div className="container-fluid" style={consoleStyle}>
-            <textarea style={editorWindow} onKeyDown={onPressKey}/>
+            <textarea style={editorWindow} /*onKeyDown={onPressKey}*/ onChange={onChangeEditor} />
         </div>
+        <div>
+            <button className="btn btn-md btn-info" onClick={onSubmitRawQuery}>Submit</button>
+        </div>
+
     </div>)
 }
 
---
+++ src/conf/api.js
@@ -0,0 +1,3 @@
+export const apiConfig = {
+    "wyrmqueryUrl": "http://localhost:5000/api/v1"
+}
